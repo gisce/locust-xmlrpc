e73d35930c076aa81fdfd7bbd4ac6032b96cc3be
Eduard Carreras <ecarreras@gisce.net>
2018-06-14 19:22:14 +0200

    Fix support pyhton3

--- locust_xmlrpc/__init__.py
+++ locust_xmlrpc/__init__.py
@@ -1,1 +1,5 @@
-from .transport import LocustXmlRpcTransport
+import six
+if six.PY2:
+    from .transport import LocustXmlRpcTransport2 as LocustXmlRpcTransport
+elif six.PY3:
+    from .transport import LocustXmlRpcTransport3 as LocustXmlRpcTransport
--- locust_xmlrpc/transport.py
+++ locust_xmlrpc/transport.py
@@ -4,7 +4,58 @@ from six.moves import xmlrpc_client
 from locust import Locust, events
 
 
-class LocustXmlRpcTransport(xmlrpc_client.Transport, object):
+class LocustXmlRpcTransport3(xmlrpc_client.Transport):
+    def single_request(self, host, handler, request_body, verbose=False):
+        # issue XML-RPC request
+        start_time = time.time()
+        try:
+            try:
+                http_conn = self.send_request(host, handler, request_body, verbose)
+                resp = http_conn.getresponse()
+                if resp.status == 200:
+                    self.verbose = verbose
+                    result = self.parse_response(resp)
+
+            except xmlrpc_client.Fault:
+                raise
+            except Exception:
+                #All unexpected errors leave connection in
+                # a strange state, so we clear it.
+                self.close()
+                raise
+
+            #We got an error response.
+            #Discard any response data and raise exception
+            if resp.status != 200:
+                if resp.getheader("content-length", ""):
+                    resp.read()
+                raise xmlrpc_client.ProtocolError(
+                    host + handler,
+                    resp.status, resp.reason,
+                    dict(resp.getheaders())
+                    )
+        except xmlrpc_client.Fault as e:
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_failure.fire(
+                request_type="xmlrpc",
+                name=handler,
+                response_time=total_time,
+                exception=e
+            )
+            return ""
+        else:
+            total_time = int((time.time() - start_time) * 1000)
+            response_length = int(resp.getheader("content-length", 0))
+            events.request_success.fire(
+                request_type="xmlrpc",
+                name=handler,
+                response_time=total_time,
+                response_length=response_length
+            )
+            return result
+
+
+class LocustXmlRpcTransport2(xmlrpc_client.Transport, object):
     def single_request(self, host, handler, request_body, verbose=0):
         start_time = time.time()
         try:
@@ -31,7 +82,7 @@ class LocustXmlRpcTransport(xmlrpc_client.Transport, object):
                 self.close()
                 raise
 
-            if result is None:
+            if response.status != 200:
                 #discard any response data and raise exception
                 if (response.getheader("content-length", 0)):
                     response.read()
