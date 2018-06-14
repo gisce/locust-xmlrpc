fa2addef7759ca717b8e460f642b0e0418cde8ec
Eduard Carreras <ecarreras@gisce.net>
2018-06-14 19:27:55 +0200

    Bump to v0.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as readme_file:
 
 setup(
     name='locust-xmlrpc',
-    version='0.2.0',
+    version='0.3.0',
     description='XML-RPC Transport for Locust',
     long_description=readme,
     author='GISCE-TI, S.L.',
